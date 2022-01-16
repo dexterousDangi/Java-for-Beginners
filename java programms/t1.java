126b53e147efd6298c0eee0140bcb7c2915daf68
desterousDangi <inboxpardeepdangi@gmail.com>
2022-01-17 00:14:08 +0530

    to understand the changes

---
+++ java programms/t1.java
@@ -0,0 +1,11 @@
+/* project name: data types
+project team size: 1
+*/
+import java.lang.*;
+class Brcm
+{
+public static void main (String [] args)
+{
+System.out.println("covid hit people");
+}
+}
