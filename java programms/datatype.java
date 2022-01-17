6c1e9e9f5d419ac12bf62acd744816581a7cc53f
desterousDangi <inboxpardeepdangi@gmail.com>
2022-01-17 14:48:46 +0530

    some predefined data types

---
+++ java programms/datatype.java
@@ -0,0 +1,17 @@
+//Predefined data types in corejava
+import java.lang.String;
+import java.lang.System;
+class Datatype
+{ 
+public static void main(String [] args)
+{
+int a = 111;
+System.out.println(a);
+double d= 1100.1;
+System.out.println(d);
+char ch='a';
+System.out.println(ch);
+boolean b=true;
+System.out.println(b);
+}
+}
