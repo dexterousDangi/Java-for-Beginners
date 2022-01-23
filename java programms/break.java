328a657952c95b13181c2cc52912b7e9c44b2319
desterousDangi <inboxpardeepdangi@gmail.com>
2022-01-23 16:59:32 +0530

    Transfer statements under control flow

---
+++ java programms/break.java
@@ -0,0 +1,14 @@
+class  Fortel
+{
+public static void main(String []args)
+{
+for (int i=0;i<0;i++)
+{
+if(i==5)
+{
+break;
+}
+System.out.println(i);
+}
+}
+}
--- java programms/sample.java
+++ java programms/sample.java
@@ -4,7 +4,9 @@ public static void main(String []args)
 {
 int a=11,b=98,c=21;
 System.out.println(a+b);
-do while(true)System.out.println("do and while statements with example");
+do 
+while(false)
+System.out.println("do and while statements with example");
 
 
 while (true);
