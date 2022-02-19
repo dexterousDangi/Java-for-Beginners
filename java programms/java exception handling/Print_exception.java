d66ea60ac3097f38d2c8defe9b197954bf1dfdf4
desterousDangi <inboxpardeepdangi@gmail.com>
2022-02-19 22:45:08 +0530

    about exception information printing

---
+++ java programms/java exception handling/Print_exception.java
@@ -0,0 +1,50 @@
+// various ways of printing exception informations
+// there are 3 ways to do so
+// using 'printStackTrace()', using 'getmessage()' and by using 'toString()'.
+
+
+class Tap
+{
+void m1()
+{
+m2();
+}
+void m2() 
+{
+m3();
+}
+void m3()
+{
+try
+{
+System.out.println(10/0);
+}
+catch (ArithmeticException ae)
+{
+System.out.println(ae.toString());
+System.out.println(ae);
+System.out.println(ae.getMessage());
+ae.printStackTrace();
+}
+} 
+public static void main(String []arg)
+{
+new Tap().m1();
+}
+}
+
+/*output:
+
+F:\java by dragon\java programms\java exception handling>javac Print_exception.j
+ava
+
+F:\java by dragon\java programms\java exception handling>java Tap
+java.lang.ArithmeticException: / by zero
+java.lang.ArithmeticException: / by zero
+/ by zero
+java.lang.ArithmeticException: / by zero
+        at Tap.m3(Print_exception.java:20)
+        at Tap.m2(Print_exception.java:14)
+        at Tap.m1(Print_exception.java:10)
+        at Tap.main(Print_exception.java:32)
+*/
