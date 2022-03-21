1c560339c0c5a6276f8fe728bc951dfafbdd8687
desterousDangi <inboxpardeepdangi@gmail.com>
2022-03-21 23:46:50 +0530

    set interface and hashset class

---
+++ java programms/collection in java/About_set_interface_and_HashSet_class.java
@@ -0,0 +1,2 @@
+// Set Interface: introduced in java 1.2 , contains two child interfaces ( sorted set and NavigableSet Interface)
+// set interface contains three implemeting classes: HashSet, LinkedHash Set, TreeSet class.
