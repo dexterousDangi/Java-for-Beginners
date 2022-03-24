869a9e642e5a96f1ca45307e5e7597a072c37c21
desterousDangi <inboxpardeepdangi@gmail.com>
2022-03-25 00:22:04 +0530

    code modified

--- java programms/collection in java/Difference_between_HashMap_and_LinkedHashMap.java
+++ java programms/collection in java/Difference_between_HashMap_and_LinkedHashMap.java
@@ -3,7 +3,8 @@
 // hashmap did not maintain insertion order 
 // while linkedhashmap maintain insertion order
 
-
+// removing an entry form LinkedhashMap using 
+//iterator, entryset(), remove()(of iterator class)
 
 import java.util.*;
 class Difference_between_HashMap_and_LinkedHashMap{
@@ -15,11 +16,35 @@ h1.put(9,"leso");
 h1.put(13, "rama");
 System.out.println(h1);
 LinkedHashMap ls= new LinkedHashMap();
-ls.put(1, "nei");
+ls.put(111, "nei");
 ls.put(2, "moto");
 ls.put(3,"maya");
 ls.put(4, "zaya");
-System.out.println(ls);
+
+Set s1=ls.entrySet();
+Iterator itr= s1.iterator();
+while(itr.hasNext())
+{
+System.out.println(itr.next());
+}
+
+
+  // removing an entry from the map data.
+//Set s2=ls.entrySet();
+Iterator itr2= (ls.entrySet()).iterator();
+while(itr2.hasNext())
+{
+Map.Entry zz =(Map.Entry)itr2.next();
+int i = (Integer)zz.getKey();
+if(i==4)
+{itr2.remove();}
+else
+{
+System.out.println(zz);
+}
+}
+
+
 }
 }
 
@@ -27,6 +52,15 @@ System.out.println(ls);
 
 F:\java by dragon\Java-for-Beginners\java programms\collection in java>java Difference_between_HashMap_and_LinkedHashMap
 
-{3=nimo, 23=neo, 13=leso, 9=rama}
-{1=nei, 2=moto, 3=maya, 4=zaya}
+{3=nimo, 23=neo, 9=leso, 13=rama}
+
+111=nei
+2=moto
+3=maya
+4=zaya
+
+111=nei
+2=moto
+3=maya
+
 */
