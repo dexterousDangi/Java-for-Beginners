71ae66d1bdac7f6b4c9be3ae24ba1a1e0ebb5a50
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-01 23:28:41 +0530

    code modified

--- java programms/01 Exercises/Star_patterns.java
+++ java programms/01 Exercises/Star_patterns.java
@@ -1,16 +1,20 @@
 // star patterns using for loops 
 
+import java.util.Scanner;
 class Star_patterns
 {
 public static void main(String []args) throws  InterruptedException
-{
-int n =4;
+{Scanner scan = new Scanner(System.in);
+System.out.println("enter the no of rows you want");
+
+int n =scan.nextInt();
+
 for (int i=1; i<=n; i++)
 {
 for (int j=1; j<=i;j++)
 {
 Thread t1=Thread.currentThread();
-t1.sleep(1000);
+t1.sleep(100);
 System.out.print(" *");
 }
 System.out.println( );
@@ -23,11 +27,33 @@ for (int i =1; i<=n; i++)
 for (int j=n ; j>=i; j--)
 {
 Thread t1=Thread.currentThread();
-t1.sleep(1000);
+t1.sleep(100);
 System.out.print(" *");
 }
 System.out.println();
 }
+
+
+
+System.out.println();
+
+for (int i=1; i<=n; i++)
+{
+for (int j=1; j<=i;j++)
+{
+System.out.print(" *");
+}
+System.out.println( );
+}
+for (int i =1; i<=n; i++)
+{
+for (int j=n-1 ; j>=i; j--)
+{
+System.out.print(" *");
+}
+System.out.println();
+}
+
 
 }
 }
@@ -38,13 +64,33 @@ F:\java by dragon\Java-for-Beginners\java programms\01 Exercises>javac Star_patt
 
 F:\java by dragon\Java-for-Beginners\java programms\01 Exercises>java Star_patterns
 
+enter the no of rows you want
+7
  *
  * *
  * * *
  * * * *
-
+ * * * * *
+ * * * * * *
+ * * * * * * *
 reverse of above string
+ * * * * * * *
+ * * * * * *
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
 
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ * * * * * *
+ * * * * * * *
+ * * * * * *
+ * * * * *
  * * * *
  * * *
  * *
