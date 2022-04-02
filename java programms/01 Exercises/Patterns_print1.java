06943a7e242689dc78baa86ff49947beff2ee6e1
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-03 03:07:45 +0530

    triangle of equals sides, and some code modified

--- java programms/01 Exercises/Patterns_print1.java
+++ java programms/01 Exercises/Patterns_print1.java
@@ -21,5 +21,39 @@ System.out.print("*");
 }
 System.out.println();
 }
+
+for(int i=1; i<=4; i++)
+{
+for (int j=1 ; j<=i; j++)
+{
+System.out.print(" ");
+}
+for(int k=4; k>=i ; k--)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+
+
 }
 }
+
+/*output:
+
+
+F:\java by dragon\Java-for-Beginners\java programms\01 Exercises>javac Patterns_
+print1.java
+
+F:\java by dragon\Java-for-Beginners\java programms\01 Exercises>java Patterns_p
+rint1
+    *
+   **
+  ***
+ ****
+*****
+ ****
+  ***
+   **
+    *
+*/
