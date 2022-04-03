6a42cb60bdaffa9964c1b1355911f5c413c7f78a
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-03 09:01:43 +0530

    integer value search using binary search algo

---
+++ java programms/01 Exercises/Binary_search_algo.java
@@ -0,0 +1,57 @@
+// selection short
+
+import java.util.*;
+class Binary_search_algo
+{
+public static void main(String []args)
+{
+ArrayList<Integer> al = new ArrayList<> ();
+al.add(34);
+al.add(24);
+al.add(4);
+al.add(45);
+al.add(3);
+al.add(14);
+al.add(7);
+
+System.out.println(al);
+Collections.sort(al);
+System.out.println("shorted collection  "+al);
+
+Integer[] num = new Integer[al.size()];
+System.out.println(num.length);
+
+al.toArray(num);
+System.out.println("shorted array "+Arrays.toString(num));
+
+Scanner scan = new Scanner(System.in);
+System.out.println("enter the no you want to search form above array");
+
+int item =scan.nextInt();
+
+
+
+int low=0;  // lower index
+int high= num.length-1;   // last index value
+int mid=0;      // middle value
+mid =(low+high)/2;
+
+while(low<=high) // till when to perform the operations
+{
+if(num[mid] ==item)
+{
+System.out.println("searched item is present at "+ mid+ "  index");
+break;}
+else if (num[mid]<item)
+{
+low = mid +1;
+}
+else
+{
+high= mid-1;
+}
+mid=(low+high)/2;
+}
+}
+}
+
